e5d07b321bd8bb009054387dd26d1f35971959e5
Christopher Sandoval Terry <115430496+Chris-SandoTerry@users.noreply.github.com>
2023-12-05 01:44:09 -0600

    Map shaders I ahte them

--- Assets/Scripts/UI/ShowHideUI.cs
+++ Assets/Scripts/UI/ShowHideUI.cs
@@ -4,6 +4,9 @@ public class ShowHideUI : MonoBehaviour
 {
     [SerializeField] KeyCode toggleKey = KeyCode.Escape;
     [SerializeField] GameObject uiContainer = null;
+
+     Inventory _inventory;
+     InventoryItem _item;
     
     void Start()
     {
@@ -12,9 +15,27 @@ public class ShowHideUI : MonoBehaviour
     
     void Update()
     {
+       
         if (Input.GetKeyDown(toggleKey))
         {
+           
             uiContainer.SetActive(!uiContainer.activeSelf);
+            
+        }
+        map();
+        
+    }
+
+    void map()
+    {
+        if (Input.GetKeyDown(toggleKey))
+        { 
+            if (_inventory.HasItem(_item) && _item.GetDisplayName() == "Map")
+            {
+
+                uiContainer.SetActive(!uiContainer.activeSelf);
+
+            }
         }
     }
 }
